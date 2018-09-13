c9dd718bd881981b6f329c2021d2eb5dc41c0e64
Sakhawat Hussain <fcssakhawat@gmail.com>
2018-09-13 19:55:44 +0600

    head and sass file edit

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,18 +1,14 @@
 <head>
     <meta charset="UTF-8">
-
-    <!-- viewport meta -->
+    <!-- meta -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="KhaniDaani - Freelance based cooking services">
     <meta name="keywords" content="KhaniDaani, khanidaani, foods, freelancing, chef, html5">
 
-
     <title>KhaniDaani - @yield ('title') </title>
 
-    <!-- inject:css -->
     <link rel="stylesheet" href="{{ URL::to('/') }}/css/materialize-stepper.css">
-
     <link rel="stylesheet" href="{{ URL::to('/') }}/css/animate.css">
     <link rel="stylesheet" href="{{ URL::to('/') }}/css/font-awesome.min.css">
     <link rel="stylesheet" href="{{ URL::to('/') }}/css/fontello.css">
@@ -21,18 +17,13 @@
     <link rel="stylesheet" href="{{ URL::to('/') }}/css/owl.carousel.css">
     <link rel="stylesheet" href="{{ URL::to('/') }}/css/slick.css">
     <link rel="stylesheet" href="{{ URL::to('/') }}/css/trumbowyg.min.css">
-    {{-- <link rel="stylesheet" href="{{ URL::to('/') }}/css/bootstrap/bootstrap.min.css"> --}}
     <link rel="stylesheet" href="{{ URL::to('/') }}/css/app.css">
     <link rel="stylesheet" href="{{ URL::to('/') }}/css/new.css">
 
     <script src="{{ URL::to('/') }}/js/vendor/jquery/jquery-1.12.3.js"></script>
 
     @stack('head-css')
-
     @stack('head-scripts')
-
-    <!-- endinject -->
-
     <!-- Favicon -->
     <link rel="icon" type="image/png" sizes="16x16" href="{{ URL::to('/') }}/images/logo/logo_icon.png">
 </head>
