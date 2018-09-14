b44d74e009442dd672231b2be9edd325e22f28dc
juned <hussainjuned99@gmail.com>
2018-09-14 18:45:58 +0600

    chefdelivery data added dynamically

---
+++ resources/views/profile/dsp_preview.blade.php
@@ -0,0 +1,66 @@
+<div class="dsp-wrap">
+    <div class="row">
+        <div class="col-xs-12 col-sm-3 col-md-12 col-lg-3 text-xs-center">
+            <a class="dsp-images" href="{{ route('profile.show', [ 'profile' => $profile->id]) }}"><img src="{{ route('home') }}/storage/images/profile_image/{{ $dsp->user->profile->profile_image }}"
+                                                 alt="dsp profile"> </a>
+            <div class="dsp-username">
+                <h5>
+                    <a href="{{ route('profile.show', [ 'profile' => $profile->id]) }}">{{ $dsp->user->name }}</a>
+                </h5>
+
+            </div>
+        </div>
+        <!--end:col -->
+        <div class="col-xs-12 col-sm-9 col-md-12 col-lg-9">
+            <a href="#"><h5 class="dsp-title">{{ $dsp->service_title }}</h5>
+            </a>
+            <div class="sell">
+                <p>
+                    <span class="bold">DSP-ID:</span>
+                    <span class="bold text-primary">{{ $dsp->id }}</span>
+                    <small class="ml-3">(Please Use This ID When Adding dsp in New Dish Upload)</small>
+                </p>
+
+            </div>
+            <p class="dsp-description">{!! $dsp->service_description !!}</p>
+            <p class="dsp-area">Service Area: <span>{{ $dsp->service_area }}</span></p>
+
+        </div>
+        <div class="col-md-12">
+            <div class="bottom-part">
+                <div class="btm-part-2">
+                    <div class="cost"><i class="fa fa-check"></i> Service charge:
+                        ৳{{ $dsp->service_charge }}
+                    </div>
+                    <div class="mins"><i class="fa fa-motorcycle"></i> {{ $dsp->min_delivery_time }} hrs - {{ $dsp->max_delivery_time }} hrs
+                    </div>
+
+                    <div class="rating product--rating">
+                        <ul>
+                            <li>
+                                <span class="fa fa-star-o"></span>
+                            </li>
+                            <li>
+                                <span class="fa fa-star-o"></span>
+                            </li>
+                            <li>
+                                <span class="fa fa-star-o"></span>
+                            </li>
+                            <li>
+                                <span class="fa fa-star-o"></span>
+                            </li>
+                            <li>
+                                <span class="fa fa-star-o"></span>
+                            </li>
+                        </ul>
+                        <span class="rating__count">(0)</span>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <!-- end:col -->
+    </div>
+    <!-- end:row -->
+</div>
+<!--end:Restaurant wrap -->
