91fe67cbf99961711fc8207d566eacb3e031d683
Sakhawat Hussain <fcssakhawat@gmail.com>
2018-09-19 19:37:32 +0600

    sdvafa

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-09-19 10:07:16.
+    * Generated on 2018-09-19 13:35:09.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
